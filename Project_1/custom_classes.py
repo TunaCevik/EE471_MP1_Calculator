73b394d94dd894531f3d7f78aeb375fe8de88b1e
Tuna Cevik <tunacevik0330@gmail.com>
2026-03-12 16:10:05 +0300

    mul function Calculator

--- Project_1/custom_classes.py
+++ Project_1/custom_classes.py
@@ -2,4 +2,5 @@ class Calculator:
     def __init__(self):
         # Başına tek alt tire koyarak "private" (internal) olduğunu belirttik
         self._current_val = 0
-
+    def mul(self, x):
+        self._current_val *= x
