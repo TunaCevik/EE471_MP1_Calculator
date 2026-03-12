2f0ca2487de7f5910772412b7d3e65951803e13f
Tuna Cevik <tunacevik0330@gmail.com>
2026-03-12 17:07:28 +0300

    main block added

--- Project_1/custom_classes.py
+++ Project_1/custom_classes.py
@@ -14,3 +14,15 @@ class Calculator:
             print("Cannot divide by zero!")
     def mul(self, x):
         self._current_val *= x
+
+if __name__ == "__main__":
+    a = Calculator()
+    a.add(5)
+    print(a._current_val)  # Output: 5
+    a.sub(2)
+    print(a._current_val)  # Output: 3
+    a.mul(4)
+    print(a._current_val)  # Output: 12
+    a.div(3)
+    print(a._current_val)  # Output: 4.0
+    a.div(0)  # Output: Cannot divide by zero!
